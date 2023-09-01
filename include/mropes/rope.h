05e512f5dd29b32255826eb6d386901fdd91a37c
Rui Maciel <rui.maciel@gmail.com>
2023-09-01 11:23:30 +0200

    Adopt .clang-format spec

--- include/mropes/rope.h
+++ include/mropes/rope.h
@@ -4,39 +4,38 @@
 #include <stddef.h>
 
 enum mrope_node_type {
-	MROPE_NODE_UNKNOWN = 0,
-	MROPE_NODE_LEAF,
-	MROPE_NODE_BRANCH
+  MROPE_NODE_UNKNOWN = 0,
+  MROPE_NODE_LEAF,
+  MROPE_NODE_BRANCH
 };
 
 struct mrope_node {
-	enum mrope_node_type type;
-	size_t weight;
+  enum mrope_node_type type;
+  size_t weight;
 };
 
 struct mrope_branch_node {
-	struct mrope_node node;
+  struct mrope_node node;
 
-	struct mrope_node * left;
-	struct mrope_node * right;
+  struct mrope_node *left;
+  struct mrope_node *right;
 };
 
 struct mrope_leaf_node {
-	struct mrope_node node;
+  struct mrope_node node;
 
-	char * text;
+  char *text;
 };
 
 struct mrope {
-	struct mrope_node * root_node;
+  struct mrope_node *root_node;
 };
 
-enum mrope_error_code
-{
-	MROPE_OK = 0,
-	MROPE_MALLOC_FAILED,
-	MROPE_UNKNOWN_NODE_TYPE,
-	MROPE_OUT_OF_RANGE
+enum mrope_error_code {
+  MROPE_OK = 0,
+  MROPE_MALLOC_FAILED,
+  MROPE_UNKNOWN_NODE_TYPE,
+  MROPE_OUT_OF_RANGE
 };
 
 typedef enum mrope_error_code mreturn_t;
@@ -49,10 +48,14 @@ mreturn_t mrope_prepend_rope(mrope_t *rope_inout, mrope_t *rope_inout_lhs);
 mreturn_t mrope_append_text(mrope_t *rope_inout, char *text_rhs);
 mreturn_t mrope_append_rope(mrope_t *rope_inout, mrope_t *rope_inout_rhs);
 mreturn_t mrope_clone(const mrope_t *rope_in, mrope_t *rope_out);
-mreturn_t mrope_concat( const mrope_t *rope_in_lhs, const mrope_t *rope_in_rhs, mrope_t *rope_out);
-mreturn_t mrope_split(const mrope_t *rope_in, const size_t index, mrope_t *rope_out_lhs, mrope_t *rope_out_rhs);
-mreturn_t mrope_insert(mrope_t *rope_inout, const size_t index, mrope_t *rope_in);
-mreturn_t mrope_delete(mrope_t *rope_inout, const size_t index, const size_t length);
+mreturn_t mrope_concat(const mrope_t *rope_in_lhs, const mrope_t *rope_in_rhs,
+                       mrope_t *rope_out);
+mreturn_t mrope_split(const mrope_t *rope_in, const size_t index,
+                      mrope_t *rope_out_lhs, mrope_t *rope_out_rhs);
+mreturn_t mrope_insert(mrope_t *rope_inout, const size_t index,
+                       mrope_t *rope_in);
+mreturn_t mrope_delete(mrope_t *rope_inout, const size_t index,
+                       const size_t length);
 
 size_t mrope_length(const mrope_t *rope_in);
 char mrope_index(const mrope_t *rope_in, const size_t index);
--- src/rope_stack.c
+++ src/rope_stack.c
@@ -4,82 +4,76 @@
 #include <stdlib.h>
 #include <string.h>
 
-mreturn_t mrope_stack_init(mrope_stack_t *stack)
-{
-	assert(stack != NULL);
+mreturn_t mrope_stack_init(mrope_stack_t *stack) {
+  assert(stack != NULL);
 
-	stack->pos = 0;
-	stack->size = 8;
-	stack->data = NULL;
-	stack->data = malloc(stack->size);
+  stack->pos = 0;
+  stack->size = 8;
+  stack->data = NULL;
+  stack->data = malloc(stack->size);
 
-	if(stack->data == NULL) {
-		stack->size = 0;
-		return MROPE_MALLOC_FAILED;
-	}
+  if (stack->data == NULL) {
+    stack->size = 0;
+    return MROPE_MALLOC_FAILED;
+  }
 
-	return MROPE_OK;
+  return MROPE_OK;
 }
 
-void mrope_stack_free(mrope_stack_t *stack)
-{
-	assert(stack != NULL);
+void mrope_stack_free(mrope_stack_t *stack) {
+  assert(stack != NULL);
 
-	if(stack->data != NULL) {
-		free(stack->data);
-		stack->data = NULL;
-	}
-	stack->pos = 0;
-	stack->size = 0;
+  if (stack->data != NULL) {
+    free(stack->data);
+    stack->data = NULL;
+  }
+  stack->pos = 0;
+  stack->size = 0;
 }
 
-mreturn_t mrope_stack_push(mrope_stack_t *stack, struct mrope_node *node)
-{
-	assert(stack != NULL);
+mreturn_t mrope_stack_push(mrope_stack_t *stack, struct mrope_node *node) {
+  assert(stack != NULL);
 
-	if(stack->pos == stack->size) {
-		enum mrope_error_code error = mrope_stack_grow_stack(stack);
-		if(error != MROPE_OK) {
-			return error;
-		}
-	}
+  if (stack->pos == stack->size) {
+    enum mrope_error_code error = mrope_stack_grow_stack(stack);
+    if (error != MROPE_OK) {
+      return error;
+    }
+  }
 
-	stack->data[stack->pos++] = node;
+  stack->data[stack->pos++] = node;
 
-	return MROPE_OK;
+  return MROPE_OK;
 }
 
-mreturn_t mrope_stack_grow_stack(mrope_stack_t *stack)
-{
-	assert(stack != NULL);
+mreturn_t mrope_stack_grow_stack(mrope_stack_t *stack) {
+  assert(stack != NULL);
 
-	const size_t new_size = stack->size << 1;
+  const size_t new_size = stack->size << 1;
 
-	struct mrope_node ** new_data = realloc(stack->data, new_size);
-	if(new_data == NULL) {
-		return MROPE_MALLOC_FAILED;
-	}
+  struct mrope_node **new_data = realloc(stack->data, new_size);
+  if (new_data == NULL) {
+    return MROPE_MALLOC_FAILED;
+  }
 
-	stack->data = new_data;
-	stack->size = new_size;
+  stack->data = new_data;
+  stack->size = new_size;
 
-	return MROPE_OK;
+  return MROPE_OK;
 }
 
-struct mrope_node * mrope_stack_top(mrope_stack_t *stack)
-{
-	assert(stack != NULL);
+struct mrope_node *mrope_stack_top(mrope_stack_t *stack) {
+  assert(stack != NULL);
 
-	return (stack->pos == 0) ?  NULL : stack->data[stack->pos-1];
+  return (stack->pos == 0) ? NULL : stack->data[stack->pos - 1];
 }
 
-mreturn_t mrope_stack_pop(mrope_stack_t *stack, struct mrope_node **node)
-{
-	assert(stack != NULL);
+mreturn_t mrope_stack_pop(mrope_stack_t *stack, struct mrope_node **node) {
+  assert(stack != NULL);
 
-	*node =  mrope_stack_top(stack);
+  *node = mrope_stack_top(stack);
 
-	if(stack->pos > 0) stack->pos--;
+  if (stack->pos > 0) stack->pos--;
 
-	return MROPE_OK;
+  return MROPE_OK;
 }
